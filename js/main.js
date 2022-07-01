cea8efbb4f2fe9d9b82e4d038f9dd71ba60cb9dc
Nikita <nikita.nikita.135@bk.ru>
2022-07-01 20:58:02 +1000

    Добавление функций

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
